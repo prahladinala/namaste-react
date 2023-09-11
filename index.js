c2cf0e4cb8b3094b72f5216f4aefb44e7294e216
Prahlad <prahladjngp@gmail.com>
2023-09-11 21:32:37 +0530

    Executable Script 🤑

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-var shell = require("shelljs");
+const { exec } = require("node:child_process");
 
-shell.exec(
-  "mkdir namaste-react && cd namaste-react && git clone https://github.com/prahladinala/simple-react-app . && npm i"
+// run the `ls` command using exec
+exec(
+  "mkdir namaste-react && cd namaste-react && git clone https://github.com/prahladinala/simple-react-app . && npm i",
+  (err, output) => {
+    // once the command has completed, the callback function is called
+    if (err) {
+      // log and return if we encounter an error
+      console.error("could not execute command: ", err);
+      return;
+    }
+    // log the output received from the command
+    console.log(
+      "Output 🚀: \n",
+      output,
+      "RUN COMMAND: cd namaste-react \n",
+      "THEN: npm start \n",
+      "YOUR REACT APP IS UP AND RUNNING: https://localhost:1234 \n"
+    );
+  }
 );
